3b4e3867f490e9207acfb2b19fb65e04a065b27c
Stefan Mejlgaard <st.mejlgaard@gmail.com>
2024-12-10 21:35:29 +0100

    Only type-ignore arg type
    Co-authored-by: Timothée Mazzucotelli <dev@pawamoy.fr>

--- src/griffe_warnings_deprecated/extension.py
+++ src/griffe_warnings_deprecated/extension.py
@@ -19,7 +19,7 @@ def _deprecated(obj: Class | Function) -> str | None:
         if decorator.callable_path in _decorators and isinstance(decorator.value, ExprCall):
             first_arg = decorator.value.arguments[0]
             try:
-                return ast.literal_eval(first_arg)  # type: ignore
+                return ast.literal_eval(first_arg)  # type: ignore[arg-type]
             except ValueError:
                 logger.debug("%s is not a static string", str(first_arg))
                 return None
